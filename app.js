a08b0c408193b4276537399c67c7da84e4757816
aybaneze <andrea270696@gmail.com>
2018-08-15 02:32:59 -0500

    arreglando html

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ console.log(distrito);
                              <button id="boton" onclick="document.getElementById('id01').style.display='block'" type="button"> Información </button>
                              <div id="id01" class="w3-modal w3-animate-zoom" onclick="this.style.display='none'">
                              <div class="w3-container w3-card w3-white w3-round w3-margin" style="width:50%;height:80%;">
-                              <p>`+restCity[i].NomRestaurant+`</p>
-                             </div>
+                              <p style="font-size:70%;">`+restCity[i].NomRestaurant+`</p>
+                              <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d15602.295979771907!2d-77.02664982127797!3d-12.14127347756992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurant+en+barranco+miraflores!5e0!3m2!1ses-419!2spe!4v1534317803722" width="80%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+                              </div>
                              </div>
                           </div> 
                           <div class="divImg">
