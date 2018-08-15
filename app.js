ce043d979c0475496e85f8aa8f5d481bc4939936
aybaneze <andrea270696@gmail.com>
2018-08-14 23:00:35 -0500

    union de json

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 
 
+// function redirect(){ 
+//   window.location= "indexInicio.html";
+// }
+// const splash = () => {
+// window.setTimeout("redirect()",2000)}
+// window.onload= splash; 
+
+let restaurant= "https://raw.githubusercontent.com/aybaneze/lim-2018-01-foodmap/master/restaurantes.json";
+let TypeFood=[];
+
+const abc = () =>{
+ fetch(restaurant)
+.then(dateJson => dateJson.json() )
+.then((restCity)=>{ 
+  let content= document.getElementById("lugares");
+  content.innerHTML="";
+  for(let i = 0 ; i<restCity.length;i++){
+     
+let distrito = restCity[i].Distrito;
+console.log(distrito);
+
+    if(OptDistrito.value == distrito )
+    {
+     content.innerHTML+=`<div class="w3-container w3-card w3-white w3-round w3-margin" style="width:95%;height:200px;" >
+                             <p style="font-size:20px;">`+ restCity[i].NomRestaurant+ `</p>
+                             <p style="font-size:20px;">`+ restCity[i].direccion+ `</p>
+                             <p style="font-size:20px;">`+ restCity[i].TipoComida+ `</p>
+                             <img src="`+restCity[i].imagen+`">
+                          <button onclick="" type="button"> Información </button>
+                          </div>`;
+     
+      
+    }
+   else
+{
+ // alert("No selecciono nada")
+}
+console.log(restCity[i].NomRestaurant)
+}});}
 
 
-const splash = () => {
-window.setTimeout("redirect()",2000)}
 
-function redirect(){ 
-  window.location= "indexInicio.html";
+let SelectDistrito = document.getElementById("SelectDistrito");
+let OptDistrito = SelectDistrito.options[SelectDistrito.selectedIndex].value;
+
+function Seleccionar(){
+  OptDistrito= document.getElementById("SelectDistrito")
+  console.log(OptDistrito.value)
 }
 
-window.onload= splash; 
+const btnBuscar= document.getElementById('btnBuscar');
+// let userText = document.getElementById('UserText').value;
+
+  
+
+btnBuscar.addEventListener('click', abc);
